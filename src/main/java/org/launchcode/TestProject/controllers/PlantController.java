e4b31c9530d9020be00ef7556319ae791e768c61
rlp390 <rlp390@hotmail.com>
2021-06-20 16:36:30 -0500

    Added delete button to plant edit page and controller to remove all plant notes along with designated plant.

--- src/main/java/org/launchcode/TestProject/controllers/PlantController.java
+++ src/main/java/org/launchcode/TestProject/controllers/PlantController.java
@@ -10,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import javax.websocket.server.PathParam;
 import java.util.List;
 import java.util.Optional;
 
@@ -139,6 +140,28 @@ public class PlantController {
         model.addAttribute("plantNotes", plantNotesRepository.findAll());
 
         return "redirect:../plants/edit";
+    }
+
+    @GetMapping("delete/{plantId}")
+    public String processPlantDelete(Model model, @PathVariable int plantId) {
+
+        Iterable<PlantNotes> plantNotes = plantNotesRepository.findAll();
+
+        for(PlantNotes note : plantNotes) {
+            System.out.println("notenote");
+            if(note.getPlantId() == plantId) {
+                plantNotesRepository.deleteById(note.getId());
+            }
+        }
+
+        plantRepository.deleteById(plantId);
+
+
+        model.addAttribute("title", "El Loggo de Planto");
+        model.addAttribute("plants", plantRepository.findAll());
+        model.addAttribute("plantNotes", plantNotesRepository.findAll());
+
+        return "redirect:/plants/edit";
     }
 
 
